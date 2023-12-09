cf98a2a41004058ec4025af2fe93dc410a12211c
kirill_tarasov <kirilltaras1792@yandex.ru>
2023-12-09 13:07:22 +0700

    add code in test_item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,10 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+test_item = Item("Смартфон", 10, 5)
+
+assert test_item.calculate_total_price() == 50
+
+Item.pay_rate = 1.5
+test_item.apply_discount()
+assert test_item.price == 15
